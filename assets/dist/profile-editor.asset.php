35778374dbcdc6e85452f5c11872d9fde7a2ce31
Dan White <dan.white1@wsu.edu>
2022-12-01 16:15:59 -0800

    Fix | Try again

--- assets/dist/profile-editor.asset.php
+++ assets/dist/profile-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element'), 'version' => 'b7309c776ca1f07eda8e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'b69bee1b00e1be98c502');
